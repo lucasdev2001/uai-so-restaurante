2b76ffe4c783312460ffbf69f95f2e0a27ddfcb7
Lucas Pereira <lucasdev2001@gmail.com>
2022-11-23 11:13:20 -0300

    add helmet

--- server/index.js
+++ server/index.js
@@ -4,22 +4,20 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const app = express();
+const helmet = require("helmet");
 const port = 3001;
 
 mongoose.connect('mongodb://localhost:27017/restaurante-uai-sô-database-dev');
 
 
 app.use(cors({
-    origin: ['http://localhost:3000/']
+  "origin": "*",
+  "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+  "preflightContinue": false,
+  "optionsSuccessStatus": 204
 }));
 
-app.use(function(req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  next();
-});
+app.use(helmet());
 
 
 const jsonParser = bodyParser.json();
